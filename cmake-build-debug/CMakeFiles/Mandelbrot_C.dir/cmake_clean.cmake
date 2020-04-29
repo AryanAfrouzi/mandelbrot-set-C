41cef036fc8ffbd63deffec1a845991a6c2408a5
AryanAfrouzi <aryanafrouzi@gmail.com>
2020-04-29 15:31:27 -0400

    Commit
    First Commit

---
+++ cmake-build-debug/CMakeFiles/Mandelbrot_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mandelbrot_C.dir/main.c.obj"
+  "Mandelbrot_C.pdb"
+  "Mandelbrot_C.exe"
+  "Mandelbrot_C.exe.manifest"
+  "libMandelbrot_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mandelbrot_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
